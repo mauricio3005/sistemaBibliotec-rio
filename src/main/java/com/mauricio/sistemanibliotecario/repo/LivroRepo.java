b8a63f4c4ce4b4f5fc7a83bc890515e449e5607b
mauricio3005 <mauriciomenezeslemos@gmail.com>
2026-07-22 14:48:19 -0300

    Correção de repositórios, dessa vez, fazendo a troca do metodo de encontrar por emprestados para o repositório de Livro

--- src/main/java/com/mauricio/sistemanibliotecario/repo/LivroRepo.java
+++ src/main/java/com/mauricio/sistemanibliotecario/repo/LivroRepo.java
@@ -4,6 +4,10 @@ import com.mauricio.sistemanibliotecario.model.Autor;
 import com.mauricio.sistemanibliotecario.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LivroRepo extends JpaRepository<Livro , Long> {
     Livro findByTitulo(String titulo);
+    List<Livro> findByEstaEmprestadoTrue();
+
 }
