2d6f288063f24ca9a1912ba7dfc4aed384867ebd
mauricio3005 <mauriciomenezeslemos@gmail.com>
2026-07-22 14:18:04 -0300

    Criação do model Autor e Usuário

--- src/main/java/com/mauricio/sistemanibliotecario/model/Autor.java
+++ src/main/java/com/mauricio/sistemanibliotecario/model/Autor.java
@@ -1,4 +1,47 @@
 package com.mauricio.sistemanibliotecario.model;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "autor_id",nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @OneToMany(mappedBy = "autor")
+    private List<Livro> livros;
+
+    public Autor(String nome) {
+        this.nome = nome;
+    }
+
+    public Autor(){}
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
 }
--- src/main/java/com/mauricio/sistemanibliotecario/model/Usuario.java
+++ src/main/java/com/mauricio/sistemanibliotecario/model/Usuario.java
@@ -1,4 +1,47 @@
 package com.mauricio.sistemanibliotecario.model;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "usuario_id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @OneToMany(mappedBy = "emprestadoA")
+    private List<Livro> emprestados;
+
+    public Usuario (String nome){
+        this.nome = nome;
+    }
+
+    public Usuario(){}
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Livro> getEmprestados() {
+        return emprestados;
+    }
+
+    public void setEmprestados(List<Livro> emprestados) {
+        this.emprestados = emprestados;
+    }
 }
